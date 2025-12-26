82a92cf1d83838fc86c281e4a0ceb829922cc403
LenyBl <blee.leny@gmail.com>
2025-12-26 23:33:32 +0100

    feat: implement FormAccount component for user authentication; update App component to conditionally render FormAccount or Navbar based on login state

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,34 @@ import Navbar from './components/Navbar'
 import Program from './components/Program'
 import NoProgram from './components/NoProgram'
 import NoLogin from './components/NoLogin'
+import FormAccount from './components/auth/FormAccount'
 
 export default function App() {
 
   const Programs = [
-    {id: 1, name: 'PPL', goal: 'Prise de masse', duration: 12, days: ['Lundi', 'Mercredi', 'Vendredi'], createdAt: '2024-01-01'},
-    {id: 2, name: 'Full Body', goal: 'Perte de graisse', duration: 8, days: ['Mardi', 'Jeudi', 'Samedi'], createdAt: '2024-02-15'},
-    {id: 3, name: 'Full Body', goal: 'Endurance musculaire', duration: 10, days: ['Lundi', 'Mardi', 'Jeudi', 'Vendredi'], createdAt: '2024-03-10'}
+    { id: 1, name: 'PPL', goal: 'Prise de masse', duration: 12, days: ['Lundi', 'Mercredi', 'Vendredi'], createdAt: '2024-01-01' },
+    { id: 2, name: 'Full Body', goal: 'Perte de graisse', duration: 8, days: ['Mardi', 'Jeudi', 'Samedi'], createdAt: '2024-02-15' },
+    { id: 3, name: 'Full Body', goal: 'Endurance musculaire', duration: 10, days: ['Lundi', 'Mardi', 'Jeudi', 'Vendredi'], createdAt: '2024-03-10' }
   ];
 
-  const isLogin = true;
+  const isLogin = false;
 
   return (
     <div className="bg-neutral-950 min-h-screen w-full text-neutral-200">
-      <Navbar />
       {
         isLogin ? (
-          Programs.length > 0 ? (
-            <Program programs={Programs} />
-          ) : (
-            <NoProgram />
-          )
+          <>
+            <Navbar />
+            {
+              Programs.length > 0 ? (
+                <Program programs={Programs} />
+              ) : (
+                <NoProgram />
+              )
+            }
+          </>
         ) : (
-          <NoLogin />
+          <FormAccount />
         )
       }
     </div>
